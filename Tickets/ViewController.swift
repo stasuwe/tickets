b1beb74f9a3766f72bd4404b67feb1d38051f0c2
Stanislav Sarychev <sarychev@tutu.ru>
2018-11-06 17:05:51 +0300

    Create project

---
+++ Tickets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tickets
+//
+//  Created by Sarychev Stanislav on 06/11/2018.
+//  Copyright © 2018 Sarychev Stanislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
